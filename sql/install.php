040d88cd8cf0b06d23b67b82174b577d7f997c7a
Prestaworks <daniel@prestaworks.se>
2019-06-19 09:05:00 +0200

    validation

--- sql/install.php
+++ sql/install.php
@@ -22,7 +22,7 @@
 *  @copyright 2007-2015 PrestaShop SA
 *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
-*/ 
+*/
 
 $sql = array();
 
