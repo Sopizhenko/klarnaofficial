73ed21fe1fd6e3861f52b587711df28eee5bcb4a
Prestaworks <daniel@prestaworks.se>
2019-12-30 13:44:27 +0100

    2.1.21

--- klarnaofficial.php
+++ klarnaofficial.php
@@ -168,7 +168,7 @@ class KlarnaOfficial extends PaymentModule
     {
         $this->name = 'klarnaofficial';
         $this->tab = 'payments_gateways';
-        $this->version = '2.1.20';
+        $this->version = '2.1.21';
         $this->author = 'Prestaworks AB';
         $this->module_key = 'b803c9b20c1ec71722eab517259b8ddf';
         $this->need_instance = 1;
@@ -3383,8 +3383,10 @@ class KlarnaOfficial extends PaymentModule
     ) {
         $delivery_address_id = 0;
         $invoice_address_id = 0;
-        $shipping_iso = $country_iso_codes[$shipping['country']];
-        $invocie_iso = $country_iso_codes[$billing['country']];
+        // $shipping_iso = $country_iso_codes[$shipping['country']];
+        // $invocie_iso = $country_iso_codes[$billing['country']];
+        $shipping_iso = Tools::strtoupper($shipping['country']);
+        $invocie_iso = Tools::strtoupper($billing['country']);
         $shipping_country_id = Country::getByIso($shipping_iso);
         $invocie_country_id = Country::getByIso($invocie_iso);
 
