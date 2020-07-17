d6331874fe6e065c1412bad7169b1da384daec3b
Prestaworks <daniel@prestaworks.se>
2020-07-17 09:21:23 +0200

    2.1.30

--- klarnaofficial.php
+++ klarnaofficial.php
@@ -205,7 +205,7 @@ class KlarnaOfficial extends PaymentModule
     {
         $this->name = 'klarnaofficial';
         $this->tab = 'payments_gateways';
-        $this->version = '2.1.29';
+        $this->version = '2.1.30';
         $this->author = 'Prestaworks AB';
         $this->module_key = 'b803c9b20c1ec71722eab517259b8ddf';
         $this->need_instance = 1;
