c916faa579023d0da4cc94d8591b327beb470d9d
Prestaworks <daniel@prestaworks.se>
2020-08-31 09:54:14 +0200

    Move the branches
    All versions have their own branch.
    Master will keep documentation

--- translations/en.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-$_MODULE['<{klarnaofficial}prestashop>klarnaofficial_d5afb631f48512f630db44a4ab35fa93'] = 'Activate KCO for this shop, if set to no, KPM will be used.';
